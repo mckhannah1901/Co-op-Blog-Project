723ca6b895dde99048efc4edde0ec5e9bf459e19
Hannah McKenna <mckhannah@eircom.net>
2020-01-21 13:34:25 +0000

    Added the landing page for the blogging engine

---
+++ landing_page/app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+    def home
+        print "Welcome to the Blogging Engine! Use the links below to\n create a user account or to login!"
+    end
+end

54050d30899459c8a7d84f209e13a052f46bb187
Hannah McKenna <mckhannah@eircom.net>
2020-01-29 10:45:48 +0000

    Implemented login functionality

--- landing_page/config/routes.rb
+++ landing_page/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  resources :users
-  get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
   root to: 'pages#home'
+
+  get 'home/index'
+
+  post '/create_account', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to:'sessions#destroy'
+  
+  resources :users
+  
 end

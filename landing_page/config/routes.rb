98a6f515f29966973838746487304946e3e1901e
mckhannah1901 <hannah.mckenna@wpengine.com>
2020-02-11 10:07:28 +0000

    minor changes to basic blogs

--- landing_page/config/routes.rb
+++ landing_page/config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to:'sessions#destroy'
+ 
+  
   
   resources :users
+
+  # resources :microposts, only: [:create, :edit, :destroy]
   
 end

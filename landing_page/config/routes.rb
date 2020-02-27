de44712713e63b4604eab82ac8d51c0b5efdb933
Hannah McKenna <hannah.mckenna@wpengine.com>
2020-02-27 16:42:46 +0000

    Fixed a routing error, implemented a flash message on successful blog post edit and redirected back to list of posts after editing

--- landing_page/config/routes.rb
+++ landing_page/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post '/create_account', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  get '/logout', to:'sessions#destroy'
+  delete '/logout', to:'sessions#destroy'
  
   
   

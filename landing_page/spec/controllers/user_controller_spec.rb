18d87ebf6662494bc3b421944d0b6e13c9e18ce9
Hannah McKenna <hannah.mckenna@wpengine.com>
2020-03-25 17:26:54 +0000

    Added additional tests to the user_controller_spec.rb file

--- landing_page/spec/controllers/user_controller_spec.rb
+++ landing_page/spec/controllers/user_controller_spec.rb
@@ -18,21 +18,67 @@ RSpec.describe UsersController, type: :controller do
             expect(response).to redirect_to(assigns(:user))
         end
 
-        private
-
-            def create_new_user
-                post :create, params: {:user => {:first_name => 'Testing', :second_name => 'Tested', :email => 'testing@tested.test', :password => 'password', :password_confirmation => 'password'}}
-            end
-
     end
 
     context "GET #index" do
 
-        it "expects a redirect sponse to be 200" do
+        it "shows the list of users, expects a redirect response to be 200" do
             get :index
             expect(response.status).to eq(200)
         end
 
     end
+
+    context "GET #show" do
+
+        it "shows a user their profile, expect redirect response to be 200" do
+            user = FactoryBot.create :user
+            user.save
+            login(user)
+
+            get :show, params: { id: user.id }
+            expect(response.status).to eq(200)
+        end
+
+    end
+
+    context "DELETE #destroy" do
+
+        it "deletes a user from the database" do
+            user = FactoryBot.create :user
+            user.save
+            login(user)
+
+            expect{delete :destroy, params: { id: user.id }}.to change(User, :count).by(-1)
+        end
+
+    end
+
+    context "PUT #update" do
+
+        it "allows a user to update their data" do
+            user = FactoryBot.create :user
+            user.save
+            login(user)
+
+            put :update, params: { :id => user.id, :user => { :first_name => "Jane", :second_name => "Doe",
+                                :email => "factorybot@test.com", :password => "password", :password => "password"}}
+            user.reload
+
+            expect(user.first_name).to eq("Jane")
+            expect(response).to redirect_to(assigns(:user))
+        end
+
+    end
+
+    private
+
+        def create_new_user
+            post :create, params: {:user => {:first_name => 'Testing', :second_name => 'Tested', :email => 'testing@tested.test', :password => 'password', :password_confirmation => 'password'}}
+        end
+
+        def login(user)
+            session[:user_id] = user.id
+        end
 
 end

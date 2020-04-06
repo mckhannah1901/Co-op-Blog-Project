e74ea4c72874bcc3af8d4a33cd35bb24d69c7205
Hannah McKenna <hannah.mckenna@wpengine.com>
2020-04-06 16:56:33 +0100

    Setup React

--- landing_page/app/javascript/packs/application.js
+++ landing_page/app/javascript/packs/application.js
@@ -22,3 +22,7 @@ import "../stylesheets/application"
 require("trix")
 
 require("@rails/actiontext")
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
--- landing_page/config/webpack/environment.js
+++ landing_page/config/webpack/environment.js
@@ -1,10 +1,5 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require("webpack")
+const typescript =  require('./loaders/typescript')
 
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-	$: 'jquery',
-	jQuery: 'jquery',
-	Popper: ['popper.js', 'default']
-}))
-
+environment.loaders.prepend('typescript', typescript)
 module.exports = environment

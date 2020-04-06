26a6f13cf0babdd05e0c1309613bcd7fa31c0b8a
Hannah McKenna <hannah.mckenna@wpengine.com>
2020-04-06 12:04:26 +0100

    Changed micropost_spec.rb tests

--- landing_page/spec/models/micropost_spec.rb
+++ landing_page/spec/models/micropost_spec.rb
@@ -6,15 +6,20 @@ RSpec.describe Micropost, type: :model do
   context 'micropost with unacceptable data' do
     it 'fails if no title is present' do
       micropost.title = ''
+      expect(micropost.title).to be_empty
       expect(micropost).to_not be_valid
     end
 
     it 'fails if no content is present' do
       micropost.content = ''
+      expect(micropost.content).to be_empty
+      expect(micropost).to_not be_valid
     end
 
     it 'fails if no user id is present' do
       micropost.user_id = ''
+      expect(micropost.user_id).to be_nil
+      expect(micropost).to_not be_valid
     end
   end
 end

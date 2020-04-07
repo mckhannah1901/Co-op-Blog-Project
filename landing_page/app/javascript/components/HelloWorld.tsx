901e5f0396875b5436a1a22297a9cbce5732622e
Hannah McKenna <hannah.mckenna@wpengine.com>
2020-04-07 14:06:47 +0100

    Changed react component in HelloWorld.tsx

---
+++ landing_page/app/javascript/components/HelloWorld.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+class HelloWorld extends React.Component {
+  render () {
+    return (
+      <div>
+          <strong><p><u>Greetings from React!</u></p></strong>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld;
